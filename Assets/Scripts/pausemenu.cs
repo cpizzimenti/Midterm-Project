fe9632fdfd579cf3026356f1c16b4dbaf0bb8aa1
cpizzimenti <courtneyl.pizzimenti@gmail.com>
2021-10-28 10:44:19 -0400

    Revert "gameover menu and script"
    This reverts commit 34cf267cbf8d026d3cf6acd12c47d780e6bf7769.

--- Assets/Scripts/pausemenu.cs
+++ Assets/Scripts/pausemenu.cs
@@ -17,7 +17,6 @@ public class pausemenu : MonoBehaviour
             TogglePause();
         }
     }
-
    public void TogglePause()
     {
         if(isPaused==true)
@@ -43,7 +42,7 @@ public class pausemenu : MonoBehaviour
             isPaused = true;
         }
 
-        // isPaused = !isPaused;//this line does the same thing as in the if/else statment; sets isPaused to the opposite of what it is rn
+       // isPaused = !isPaused;//this line does the same thing as in the if/else statment; sets isPaused to the opposite of what it is rn
 
     }
     public void Quit()
