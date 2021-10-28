6edc8b434a9f11b16466e9193a76c081d0a5f948
Maveric M <mmuscat@ltu.edu>
2021-10-28 11:49:51 -0400

    gameOver script added
    Added gameOverCube and script that detects ball collision and then sends user to gameOver scene.

---
+++ Assets/Scripts/gameOver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour
+{
+
+    public void LoadLevel(string name)//function with string parameter, void is return type.
+    {
+        SceneManager.LoadScene(name);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        LoadLevel("gameover");
+    }
+
+}
